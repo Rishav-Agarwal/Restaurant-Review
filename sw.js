f743186cae71b57f9d364fc6ff0928d10a4ecdf6
Rishav Agarwal <rishavcoder@gmail.com>
2018-11-22 00:32:19 +0530

    fix: Caching and horizontal scroll

--- sw.js
+++ sw.js
@@ -4,17 +4,8 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName).then(cache => {
             return cache.addAll([
+                './',
                 './index.html',
-                './restaurant.html?id=1',
-                './restaurant.html?id=2',
-                './restaurant.html?id=3',
-                './restaurant.html?id=4',
-                './restaurant.html?id=5',
-                './restaurant.html?id=6',
-                './restaurant.html?id=7',
-                './restaurant.html?id=8',
-                './restaurant.html?id=9',
-                './restaurant.html?id=10',
 				'./css/styles.css',
 				'./data/restaurants.json',
 				'./js/dbhelper.js',
